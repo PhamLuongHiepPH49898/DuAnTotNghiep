a7d21598707547f592178ec8a348a6dae2628de2
hoi han IT <khoindph49713@gmail.com>
2025-06-01 20:07:56 +0700

    demo CRUD quan ly san bong

--- src/main/java/com/example/datn/Repository/SanBongRepo.java
+++ src/main/java/com/example/datn/Repository/SanBongRepo.java
@@ -3,8 +3,15 @@ package com.example.datn.Repository;
 import com.example.datn.Entity.SanBong;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.relational.core.sql.In;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 
 public interface SanBongRepo extends JpaRepository<SanBong, Integer>, JpaSpecificationExecutor<SanBong> {
+    @Query("SELECT s FROM SanBong s WHERE s.trang_thai IN :trangThaiList")
+    List<SanBong> findByTrangThaiIn(@Param("trangThaiList") List<Integer> trangThaiList);
 
 }
