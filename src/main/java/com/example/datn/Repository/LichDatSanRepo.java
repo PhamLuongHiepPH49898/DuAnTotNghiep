8c3ae92a28e2e84e9577596dec5c8fb93d8ccd23
nguyenngocanh05 <anhntnph49638@gmail.com>
2025-06-23 12:51:52 +0700

    check lại phần lịch trống

--- src/main/java/com/example/datn/Repository/LichDatSanRepo.java
+++ src/main/java/com/example/datn/Repository/LichDatSanRepo.java
@@ -2,6 +2,12 @@ package com.example.datn.Repository;
 
 import com.example.datn.Entity.LichDatSan;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface LichDatSanRepo extends JpaRepository<LichDatSan, Integer> {
+    @Query("SELECT s FROM LichDatSan s WHERE s.trangThai IN :trangThaiList")
+    List<LichDatSan> findByTrangThaiIn(@Param("trangThaiList") List<Integer> trangThaiList);// thêm
 }
