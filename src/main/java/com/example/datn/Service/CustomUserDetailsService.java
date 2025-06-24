79ffce1e7ce7f2f2332d03cc1a8d4533f4bb1f6c
cowtcode <thangreal22@gmail.com>
2025-06-24 16:43:23 +0700

    Ds user + đổi pass

--- src/main/java/com/example/datn/Service/CustomUserDetailsService.java
+++ src/main/java/com/example/datn/Service/CustomUserDetailsService.java
@@ -29,14 +29,23 @@ public class CustomUserDetailsService implements UserDetailsService {
                 });
 
         // Kiểm tra trạng thái tài khoản (giả sử 1 là ACTIVE)
-        if (taiKhoan.getTrang_thai() != 0) { // Thay giá trị 1 bằng giá trị tương ứng với trạng thái "ACTIVE" trong DB
-            logger.error("Tài khoản không hoạt động: {}", email);
-            throw new UsernameNotFoundException("Tài khoản không hoạt động: " + email);
-        }
 
-        logger.info("Tài khoản tìm thấy: {}", taiKhoan.getEmail());
-        logger.debug("Mật khẩu từ DB: {}", taiKhoan.getMat_khau());
-        logger.debug("Vai trò: ROLE_{}", taiKhoan.getVai_tro());
+        if (taiKhoan.getTrangThai() != 0) { // Thay giá trị 1 bằng giá trị tương ứng với trạng thái "ACTIVE" trong DB
+
+            if (taiKhoan.getTrangThai() != 0) { // Thay giá trị 1 bằng giá trị tương ứng với trạng thái "ACTIVE" trong DB
+
+                logger.error("Tài khoản không hoạt động: {}", email);
+                throw new UsernameNotFoundException("Tài khoản không hoạt động: " + email);
+            }
+
+            logger.info("Tài khoản tìm thấy: {}", taiKhoan.getEmail());
+
+            logger.debug("Mật khẩu từ DB: {}", taiKhoan.getPassword());
+            logger.debug("Vai trò: ROLE_{}", taiKhoan.getVaiTro());
+
+
+        }
         return new CustomUserDetails(taiKhoan);
     }
 }
+
